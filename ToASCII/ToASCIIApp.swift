5cf4f9e73a630791b8bfda1902ebf54eb2e2fd00
Emanuele-web04 <emanueledipietro612@gmail.com>
2024-06-22 20:07:29 +0200

    Initial Commit

---
+++ ToASCII/ToASCIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToASCIIApp.swift
+//  ToASCII
+//
+//  Created by Emanuele Di Pietro on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ToASCIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
